8863e5d542ae6cfa4bbeb7608488ed4736c8bbc3
Gilberto Silva <betosilva.luz@gmail.com>
2023-10-30 20:24:07 -0300

    Adicionando imagens e carregando lista de itens.

--- Onboarding/Onboarding/Views/Onboarding/OnboardingCollectionViewCell.swift
+++ Onboarding/Onboarding/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
+    static let identifier = String(describing: OnboardingCollectionViewCell.self)
     
     @IBOutlet weak var slideImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
--- Onboarding/Onboarding/Views/Onboarding/OnboardingViewController.swift
+++ Onboarding/Onboarding/Views/Onboarding/OnboardingViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nectButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
@@ -17,9 +17,33 @@ class OnboardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slides = [
+            OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
+            OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
+            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
+        ]
     }
     
     @IBAction func nextButtonClicked(_ sender: Any) {
         
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegate,
+                                    UICollectionViewDataSource,
+                                    UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
 }
